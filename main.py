4a2bd7cef2872b4479ae90d843f230581b782528
justyna <276587@student.pwr.edu.pl>
2024-04-17 16:18:18 +0200

    etap 4

--- main.py
+++ main.py
@@ -12,11 +12,7 @@ FAILURE = "F"
 
 # Defining variables
 file_name = 'config.txt'
-#maintance_probability_d1 = 0.9
-#maintance_probability_d2 = 0.95
 
-
-#simulation_time = 0
 #Reading from file
 def read_file(file_name):
     maintance_policy = []
@@ -30,6 +26,7 @@ def read_file(file_name):
     return maintance_policy
 
 maintance_policy = read_file(file_name)
+
 #Parameters
 transition_intencity01 = int(maintance_policy[0])
 transition_intencity12 = int(maintance_policy[1])
@@ -41,16 +38,16 @@ maintance_range2 = int(maintance_policy[6])
 
 def inspection(state):
     if state == STATE_D1:
+        print("Car inspection")
         random_number = random.randint(0, 100)
         if random_number <= decision_probability1:
-            print("Car inspection")
             return True
         else:
             return False
     if state == STATE_D2:
+        print("Car inspection")
         random_number = random.randint(0, 100)
         if random_number <= decision_probability2:
-            print("Car inspection")
             return True
         else:
             return False
@@ -59,10 +56,10 @@ def inspection(state):
 def maintance(state):
     if state == STATE_D1:
         print("The car is in state D1 and needs maintenance.")
-        state = INITIAL_STATE
+        return INITIAL_STATE
     elif state == STATE_D2:
         print("The car is in state D2 and needs maintenance.")
-        state = STATE_D1
+        return STATE_D1
 
 
 
@@ -76,13 +73,13 @@ def mainLoop():
         elif state == STATE_D1:
             print("The car is in state D1.")
             if inspection(state):
-                maintance(state)
+                state = maintance(state)
             else:
                 state = STATE_D2
         elif state == STATE_D2:
             print("The car is in state D2.")
             if inspection(state):
-                maintance(state)
+                state = maintance(state)
             else:
                 state = FAILURE
 
