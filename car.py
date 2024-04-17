29aae3ac233cb4f8eb7ecfd3e41cc050dd649f37
Kyrylo Semenchenko <kyrylo.semenchenko@gmail.com>
2024-04-17 12:28:29 +0200

    add basics of etap 4 simulation

--- car.py
+++ car.py
@@ -6,37 +6,15 @@ class Car:
     Represents a delivery car.
 
     Attributes:
-        state (float): The current state of the car.
-        reliability (int): The reliability score of the car.
-        maintain_time (int): The time interval between maintenance.
-        gigadamagerate (int): The upper range for bigger damage.
+        state (string): The current state of the car.
     """
 
-    def __init__(self, state, reliability, maintain_time, gigadamagerate):
+    def __init__(self, state):
         """
         Initializes a new Car instance.
 
         Parameters:
-            state (float): The initial state of the car.
-            reliability (int): The reliability score of the car.
-            maintain_time (int): The time interval between maintenance.
-            gigadamagerate (int): The upper range for bigger damage.
+            state (string): The initial state of the car.
+
         """
         self.state = state
-        self.reliability = reliability
-        self.maintain_time = maintain_time
-        self.gigadamagerate = gigadamagerate
-
-    def damage(self):
-        """
-        Simulates regular wear and tear on the car.
-        """
-        self.state -= 0.5  # Adjust as per actual production rate.
-
-    def gigadamage(self):
-        """
-        Simulates a significant damage occurrence on the car.
-        """
-        random_gigadamge = random.randint(1, self.gigadamagerate)
-        self.state -= random_gigadamge
-        print("A significant damage occurred: " + str(random_gigadamge) + "%")
--- main.py
+++ main.py
@@ -4,35 +4,61 @@ from car import Car
 # Introduction
 print("Welcome to Delivery Car Simulation")
 
-# Simulation parameters input
-simulation_duration = int(input("Enter simulation duration (in iterations): "))
-inspection_frequency = int(input("Enter inspection frequency: "))
-inspection_counter = inspection_frequency
 
-# Creating car object
-initial_state = 100.0
-reliability = int(input("Enter reliability score: "))
-maintain_time = 1
-gigadamagerate = int(input("Enter upper range for bigger damage: "))
-car_1 = Car(initial_state, reliability, maintain_time, gigadamagerate)
+# Defining constans
+INITIAL_STATE = "D0"
+STATE_D1 = "D1"
+STATE_D2 = "D2"
+FAILURE = "F"
 
-# Simulation loop
-for i in range(simulation_duration):
-    if inspection_counter == i:
-        if car_1.state <= 50:
-            print("Repairing process initiated")
-        inspection_counter += inspection_frequency
+# Defining variables
+maintance_probability_d1 = 0.9
+maintance_probability_d2 = 0.95
 
-    if car_1.state <= 0:
-        print("The car has reached a critical damage level and is broken.")
-        break
+simulation_time = 0
+# Main loop
+def mainLoop():
+    state = INITIAL_STATE
+    while state != FAILURE:
+        if state == INITIAL_STATE:
+            print("The car is in the initial state.")
+            state = STATE_D1
+        elif state == STATE_D1:
+            print("The car is in state D1.")
+            if inspection(state):
+                maintance(state)
+            else:
+                state = STATE_D2
+        elif state == STATE_D2:
+            print("The car is in state D2.")
+            if inspection(state):
+                maintance(state)
+            else:
+                state = FAILURE
 
-    print("Car State: " + str(car_1.state) + "%")
-    car_1.damage()
-    rand = random.randint(0, 100)
 
-    if rand in range(0, car_1.reliability):
-        car_1.gigadamage()
+def inspection(state):
+    if state == STATE_D1:
+        random_number = random.randint(0, 10)
+        if random_number <= maintance_probability_d1 * 10:
+            state = INITIAL_STATE
+            return True
+        else:
+            return False
+    if state == STATE_D2:
+        random_number = random.randint(0, 100)
+        if random_number <= maintance_probability_d2 * 100:
+            return True
+        else:
+            state = FAILURE
+            return False
 
-# todo zużycie z upłyłem czasu (zależny od producenta)
-# todo rozszerzenie na całą flotę pojazdów
+
+# Defining functions
+def maintance(state):
+    if state == STATE_D1:
+        print("The car is in state D1 and needs maintenance.")
+        state = INITIAL_STATE
+    if state == STATE_D2:
+        print("The car is in state D2 and needs maintenance.")
+        state = STATE_D1
